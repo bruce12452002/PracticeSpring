ec2614d8ba633321ae70024f4ce9244b52e9a5c7
bruce <bruce12452002@yahoo.com.tw>
2018-08-16 15:44:00 +0800

    add junit test autowired

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,10 +1,31 @@
 
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import bean.Animal;
+import bean.Bird;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = Animal.class)
 public class LibraryTest {
+	@Autowired
+	private Animal a;
+
+	@Autowired
+	private Bird b;
+
+	@Test
+	public void testAutowired() {
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+	@Ignore
 	@Test
 	public void testSpring() {
 		AnnotationConfigApplicationContext app = new AnnotationConfigApplicationContext(Animal.class);
